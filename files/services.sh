b5c2c2eb81db21d2c722d847e1b9b719b5186cf3
core@core1 <core@core1>
2014-11-14 18:03:10 +0900

    web用ディレクトリは不要なので削除

--- files/services.sh
+++ files/services.sh
@@ -5,13 +5,5 @@ if [ ! -L $LOCALTIME ]; then
   ln -s /usr/share/zoneinfo/Asia/Tokyo $LOCALTIME
 fi
 
-HOSTNAME=`hostname`
-ROOT=/var/www/$HOSTNAME
-HTML=$ROOT/html
-if [ ! -e $HTML ]; then
-	mkdir -p $HTML
-fi
-chown -R nginx: $ROOT
-
 /etc/init.d/crond start
 /usr/bin/tail -f /dev/null
