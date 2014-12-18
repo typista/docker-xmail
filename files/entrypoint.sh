aed4d4a36151c045fe61ab2f88994d9cf94c1ba3
core@core1 <core@core1>
2014-12-18 12:59:52 +0900

    exec1st.shは実行環境のexport配下の問題だった

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -6,19 +6,13 @@ DOWNLOADS="exec1st.sh startXMail.sh crontab.txt domainadd.sh useradd.sh changepa
 for FILE in $DOWNLOADS
 do
     DONE=$EXPORT/$FILE
-    if [ ! -f $DONE -a ! -s $DONE ];then
+    if [ ! -f $DONE ];then
         wget $URL/$FILE -O $DONE
     fi
     if [ ! -x $DONE ];then
         chmod +x $DONE
     fi
 done
-
-EXEC1ST=$EXPORT/exec1st.sh
-wget $URL/exec1st -O $EXEC1ST
-if [ ! -x $EXEC1ST ];then
-    chmod +x $EXEC1ST
-fi
 
 $EXEC1ST
 crontab $EXPORT/crontab.txt
