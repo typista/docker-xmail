42e38a80e9bc99851dfce2d7d797f880666e5c3f
core@core1 <core@core1>
2015-01-06 15:12:40 +0900

    /var/MailRootのシンボリックリンク化抜け

--- files/startXMail.sh
+++ files/startXMail.sh
@@ -3,6 +3,14 @@ XMAIL=/etc/init.d/xmail
 
 TEMP=/root/ps.tmp
 ps aux > $TEMP
+MAIL_ROOT_VAR=/var/MailRoot
+MAIL_ROOT_EXPORT=/root/export/MailRoot
+if [ ! -L $MAIL_ROOT_VAR -a -e $MAIL_ROOT_EXPORT ]; then
+	if [ -e $MAIL_ROOT_VAR ]; then
+		rm -rf $MAIL_ROOT_VAR
+	fi
+	ln -s $MAIL_ROOT_EXPORT $MAIL_ROOT_VAR
+fi
 IS_XMAIL=`cat $TEMP | grep -v 'null' | grep '/var/MailRoot/bin/XMail'`
 if [ -f $XMAIL -a "$IS_XMAIL" = "" ];then
     chmod +x $XMAIL
